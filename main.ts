3c3022161744a247ebdff3e839f4fcbba0bc7f94
Niels Louwes <nielslouwes45@gmail.com>
2025-04-04 11:36:37 +0200

    ts fix

--- main.ts
+++ main.ts
@@ -1,29 +1,26 @@
 // @deno-types="npm:@types/express@4"
 import express, { NextFunction, Request, Response } from "express";
-import { DataLayerEventType } from "./types";
+import cors from "npm:cors";
+import { DataLayerEventType } from "./types/types.ts";
 
 // GOAL: Store events, send back failure or success so FE event buffer service can process
 const app = express();
 const port = 8000;
 
+app.use(cors());
 app.use(express.json());
 
 const events: DataLayerEventType[] = [];
 
 app.get("/", (_req, res) => {
-  console.log("res", res);
-  console.log("_req", _req);
   res.status(200).send("Welcome to the event backend service!");
-});
-
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
 });
 
 app.post("/events", (_req, res) => {
   try {
     // Log the incoming event for debugging
     console.log("Received event:", _req.body);
+    console.log("response", res);
 
     events.push(_req.body);
 
@@ -38,4 +35,8 @@ app.post("/events", (_req, res) => {
       error: "Failed to process event",
     });
   }
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
 });
